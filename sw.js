2f3354a54afb4a23b77869e0022e0b93cd1d7aa2
yehasun abid <84736240+yehasun@users.noreply.github.com>
2025-09-10 23:05:59 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = "news-card-cache-v1";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./output.html",
+  "./style.css",
+  "./img/demo.png",
+  "./img/demo2.jpg",
+  "./manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
